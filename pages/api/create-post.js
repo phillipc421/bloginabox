09576d23db73f05f46f55a6632c01fdeeeced877
madmonkey <104393122+phillipc421@users.noreply.github.com>
2023-05-18 00:34:03 -0500

    finished endpoint

--- pages/api/create-post.js
+++ pages/api/create-post.js
@@ -1,29 +1,7 @@
 import { readFile, writeFile } from "fs/promises";
 import path from "path";
-
-export default async function handler(req, res) {
-	const { method } = req;
-	if (method !== "POST") {
-		return res.status(400).json({ message: "Invalid endpoint" });
-	}
-
-	try {
-		// load existing file
-		const existingData = await readFile(
-			path.join(process.cwd(), process.env.POSTS_FILE),
-			"utf-8"
-		);
-		const parsedData = JSON.parse(existingData);
-		// validate
-		const { title, body, author } = req.body;
-
-		// write file
-		return res.status(201).json({ message: parsedData });
-	} catch (e) {
-		console.error(e);
-		return res.status(500).json({ message: "Internal Server Error" });
-	}
-}
+import { createPost } from "../../lib/createPost";
+import { validate } from "../../lib/validate";
 
 /*
 post schema
@@ -36,3 +14,44 @@ post schema
     updatedOn: Date - auto
 } 
 */
+
+export default async function handler(req, res) {
+  const { method } = req;
+  if (method !== "POST") {
+    return res.status(400).json({ message: "Invalid endpoint" });
+  }
+
+  try {
+    // validate
+    const errorString = validate(req.body);
+    if (errorString) {
+      return res.status(400).json({ message: errorString });
+    }
+
+    // create post
+    const newPost = createPost(req.body);
+
+    // load existing file
+    const existingData = await readFile(
+      path.join(process.cwd(), process.env.POSTS_FILE),
+      "utf-8"
+    );
+    const posts = JSON.parse(existingData);
+
+    // modify posts object
+    posts[newPost.id] = newPost;
+
+    // write file
+    const postsString = JSON.stringify(posts);
+    await writeFile(
+      path.join(process.cwd(), process.env.POSTS_FILE),
+      postsString,
+      "utf-8"
+    );
+
+    return res.status(201).json({ message: "Post created", data: newPost });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
